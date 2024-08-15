0a8be71034a7976b457c417f6af570e203036e9d
Nikert <2906026@mail.ru>
2024-08-15 16:17:52 +0300

    init

--- src/JavaTask2.java
+++ src/JavaTask2.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 
 public class JavaTask2 {
     public static void main(String[] args) {
-        findingVowels(
+        findingPhoneNumbers(
                 "asd+375(17)1234567asd " +
                 "+375291234567 " +
                 "da88 029 123 45 67f" +
@@ -14,7 +14,7 @@ public class JavaTask2 {
                 "gfh+375 29 123 45 67");
     }
 
-    public static void findingVowels (String text) {
+    public static void findingPhoneNumbers (String text) {
         Pattern pattern = Pattern.compile("((\\+375|80|8 0)( ?(29|25|17|33|44) ?| ?\\((29|25|17|33|44)\\) ?)(\\d{7}|\\d{3}([- ])\\d{2}\\7\\d{2}))");
         Matcher matcher = pattern.matcher(" " + text);
         while (matcher.find()) {
